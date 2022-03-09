c9f95a07298459534648951c7bac127bb6b672a2
@Rossetal <uh1hmx@yahoo.com>
2022-03-09 12:44:49 -0600

    se agregaros archivos if y else condicionales

---
+++ ternario.js
@@ -0,0 +1,19 @@
+let edad = 15;
+let esMayorDeEdad;
+edad >= 18 ? esMayorDeEdad = true : esMayorDeEdad = false
+alert(`Es mayor de edad: ${esMayorDeEdad ? 'si' : 'no'}` )
+//Ternario
+//edad >= 18 ? alert("Eres mayor de edad") : alert("Eres menor de edad");
+//condicion> ? que pasa si se cumple : <que pasa si no se cumple></que>
+// if(edad >= 18){
+//     alert("Eres mayor de edad");
+// }else{
+//     alert("Eres menor de edad");
+// }
+
+//if
+// if(<condicion>){
+ //   <que pasa si se cumple>
+//}else{
+  //  { <que pasa si no se cumple>
+  // } 
