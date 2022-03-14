63d78bbcb63f3c02b909cfde76c51df755a2c1e2
@Rossetal <uh1hmx@yahoo.com>
2022-03-13 20:02:39 -0600

    se agrego archivo Arrays

---
+++ 3_arrays/app.js
@@ -0,0 +1,30 @@
+
+//Todos los elementos que no sean primirtivos bienen de Objet
+//por lo tanto un Array es un Objet
+
+//Un Array es un lista de elementos del mismo tipo o distintos tipos que podemos manipular (añadie,quitar,ordenar)
+//var alumnos = ['Ana','Jorge','Diego'];
+var alumnos = ['Ana', 'Jorge', 'Diego'];
+console.log(alumnos);
+
+var edades = [1, 200, 300, 0];
+console.log(edades);
+
+var info = ['hola', 1, true, 200, 'jeje'];
+console.log(info);
+
+//Acceder a un elementod ela lista
+var precios = [100, 188, 288, 20, 33];
+console.log(precios);
+//Para acceder a un elemento necesito conoser su posicion tomando en cuenta que las posiciones inican en 0
+var precios = [100, 188, 288, 20, 33];
+console.log(precios);
+//Por lo tanto el 100 esta en la psicion 0
+// 188 seria la posicion 1
+console.log(precios[2]);
+console.log(alumnos[2]);
+
+//Metodos de los arreglos leer (todolo que puede ser aplicado a un arreglo)
+
+
+
