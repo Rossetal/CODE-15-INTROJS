2b1370bbf3889c8a8acf6342294165bfe64c1af6
@Rossetal <uh1hmx@yahoo.com>
2022-03-13 18:57:32 -0600

    se agregaaron ejercicios condicionales

---
+++ 2_repaso-conceptos/Ejercicios_condicionales.js
@@ -0,0 +1,47 @@
+// Arnoldo le gustan mucho los números y quiere saber más de ellos, él denomina
+// números cool a los que son divisibles entre 5, el quiere crear un programa en el
+// cual el ingrese un número y le diga si es cool o no, ¿puedes ayudarlo?
+/* var numero = 10;
+if(numero%5 === 0){
+    console.log('Es un numero cool');
+}else{
+    console.log('No es cool');
+}
+ */
+
+// Humberto está repartiendo códigos por toda la ciudad. El quiere crear un programa
+// que cuando un usuario ingrese un código este despliegue un mensaje. Para el
+// código "happy" el quiere desplegar el mensaje "cool bro, the life is short" y para el
+// código "sad" él quiere, desplegar el mensaje "go for tacos bro, tacos = smile"
+// ** Desplegar el mensaje "código inválido" si el código no cumple con las condiciones anteriores
+/* var codigo = 'Happy';
+    if(codigo === 'Happy'){
+        console.log("cool bro, the life is short");
+    }else if(codigo === 'sad'){
+        console.log('go for tacos bro, tacos = smile')
+    }else{
+        console.log('Codigo Invalido');
+    } */
+
+// Paco está haciendo un videojuego y necesita hacer un programa que determine la
+// jerarquía de usuario de acuerdo a su nivel, las jerarquías son "paladin" : nivel 0 -10,
+// "caballero dorado" : nivel 11 - 30, "dios destructor": 31 - 50.
+/* var nivel = -100;
+if(nivel <= 10 && nivel >= 0){
+    console.log('paladin');
+}else if(nivel <= 30 && nivel >=11){
+    console.log('Caballero dorado')
+}else if(nivel <= 50 && nivel >=31){
+    console.log('Dios destructor')
+}else{
+    console.log('nivel invalido')
+} */
+
+//Si utlilizamos un->AND && nesecitamos que todas las condiciones se cumplan para entregar un TRUE
+
+// Crea una aplicación web empleando JavaScript que permita a dos usuarios jugar una partida de piedra, papel o tijeras.
+
+
+// Pide al jugador1 y al jugador2 que escriban su elección mediante prompts. Compara los valores ingresados y muestra en pantalla, mediante un alert o console.log, quién fue el jugador ganador. && ||
+
+// Crea una aplicación web con JavaScript que reciba tres números digitados por el usuario, mediante prompts, y muestre en pantalla, mediante un alert, cuál es el mayor de los tres números. 
