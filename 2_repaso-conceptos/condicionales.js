f04e50f148b18a843e8ab5683ce9bdb281d7b4d1
@Rossetal <uh1hmx@yahoo.com>
2022-03-13 18:55:40 -0600

    se agregaron ejemplos condicionales

---
+++ 2_repaso-conceptos/condicionales.js
@@ -0,0 +1,43 @@
+// IF 
+if(10 < 5){
+console.log('Si secumple la condicion');
+}
+
+if('Hola' === 'Hola'){
+    console.log('Si secumple la  segunda condicion');
+    }
+    var edad = 20;
+    if(edad >= 18){
+        console.log(' puedes pasar');
+    }
+//IF ELSE ->Tengo una condicion para ralizar una accion y ademas tengo una aciion en caso que la condicion no se cumpla
+var edadAlumna = 10;
+if(edadAlumna > 12){
+    console.log('Eres adolecente');
+}else{
+    console.log('Eres una niña');
+}
+
+var nombre = 'Mike';
+if(nombre === 'Luis') {
+    console.log('Hola Luis');
+}else{
+    console.log('Hola extraño');
+}
+
+
+//IF ELSE IF -> Para tener mas de una condicion
+var nombreAlunmo = 'A';
+if(nombreAlunmo === 'Jorge'){
+    console.log('Eres el ganador');
+}else if(nombreAlunmo === 'Luis'){
+    console.log('Ganaste el segundo lugar');
+} if(nombreAlunmo === 'Rogelio'){
+    console.log('Eres el tercer lugar')
+}else{
+    console.log('No has ganado un lugar')
+}
+
+
+
+//Ternario
